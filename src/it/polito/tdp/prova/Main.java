7d6ff983ce64f78fbadc070158d5eec10aeb47ae
Tiberio <Tiberio@192.168.1.133>
2017-03-19 18:35:27 +0100

    56

--- src/it/polito/tdp/prova/Main.java
+++ src/it/polito/tdp/prova/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,12587,78);
+			Scene scene = new Scene(root,1255687,78);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
